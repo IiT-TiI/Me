9402b7dd830504040c2038c1b12d68ffaea53eb0
windbird <windbird@windbird.com>
2021-08-25 15:25:51 +0800

    加入vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import {provide,ref} from 'vue'
+import {router} from './router'
+
+export default {
+  name: 'App',
+  setup(){
+    const width = document.documentElement.clientWidth
+    const asideVisiable = ref(width > 500 ? true:false)
+    provide('asideVisiable',asideVisiable)
+    router.afterEach(()=>{
+      if (width>500) {
+        return
+      }
+      asideVisiable.value=false
+    })
+  }
+}
+</script>
